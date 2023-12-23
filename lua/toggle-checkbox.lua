c7fadb2fe91491176d2d47df2c4980090d49f0ac
phancb <nxhsdu@qq.com>
2023-12-23 23:25:41 +0800

    refactor: keep it simple

--- lua/toggle-checkbox.lua
+++ lua/toggle-checkbox.lua
@@ -1,43 +1,29 @@
-local checked_character = "x"
+local CHECK_BOX = "%[x%]"
+local EMPTY_BOX = "%[ %]"
 
-local checked_checkbox = "%[" .. checked_character .. "%]"
-local unchecked_checkbox = "%[ %]"
-
-local line_contains_an_unchecked_checkbox = function(line)
-	return string.find(line, unchecked_checkbox)
+local toggle_line = function(line)
+	local has = function(sym) return line:find("^%s*- " .. sym) or line:find("^%s*%d%. " .. sym) end
+	local check = function() return line:gsub(EMPTY_BOX, CHECK_BOX, 1) end
+	local clear = function() return line:gsub(CHECK_BOX, EMPTY_BOX, 1) end
+	local make_box = function()
+		if line:match("^%s*-%s.*$") then return line:gsub("(%s*- )(.*)", "%1[ ] %2", 1) end
+		if line:match("^%s*%d%s.*$") then return line:gsub("(%s*%d%. )(.*)", "%1[ ] %2", 1) end
+		return line:gsub("(%S+)", "- [ ] %1", 1)
+	end
+	if has(CHECK_BOX) then return clear() end
+	if has(EMPTY_BOX) then return check() end
+	return make_box()
 end
 
-local checkbox = {
-	check = function(line)
-		return line:gsub(unchecked_checkbox, checked_checkbox)
-	end,
-
-	uncheck = function(line)
-		return line:gsub(checked_checkbox, unchecked_checkbox)
-	end,
-}
-
 local M = {}
-
 M.toggle = function()
-	local bufnr = vim.api.nvim_buf_get_number(0)
 	local cursor = vim.api.nvim_win_get_cursor(0)
-	local start_line = cursor[1] - 1
-	local current_line = vim.api.nvim_buf_get_lines(bufnr, start_line, start_line + 1, false)[1] or ""
-
-	-- If the line contains a checked checkbox then uncheck it.
-	-- Otherwise, if it contains an unchecked checkbox, check it.
-	local new_line = ""
-	if line_contains_an_unchecked_checkbox(current_line) then
-		new_line = checkbox.check(current_line)
-	else
-		new_line = checkbox.uncheck(current_line)
-	end
-
-	vim.api.nvim_buf_set_lines(bufnr, start_line, start_line + 1, false, { new_line })
+	local lineid = cursor[1] - 1
+	local line = vim.api.nvim_buf_get_lines(0, lineid, lineid + 1, false)[1] or ""
+	local new_line = toggle_line(line)
+	vim.api.nvim_buf_set_lines(0, lineid, lineid + 1, false, { new_line })
 	vim.api.nvim_win_set_cursor(0, cursor)
 end
 
-vim.api.nvim_create_user_command("ToggleCheckbox", M.toggle, {});
-
+vim.api.nvim_create_user_command("ToggleCheckbox", M.toggle, {})
 return M
