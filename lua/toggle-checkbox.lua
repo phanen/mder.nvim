f0c668b27cbc7aa82df14d4d4774450a18f76d2d
phancb <nxhsdu@qq.com>
2023-12-24 15:28:46 +0800

    feat: support visual mode

--- lua/toggle-checkbox.lua
+++ lua/toggle-checkbox.lua
@@ -1,29 +1,38 @@
 local CHECK_BOX = "%[x%]"
 local EMPTY_BOX = "%[ %]"
+local PREFIX = "-"
 
 local toggle_line = function(line)
-	local has = function(sym) return line:find("^%s*- " .. sym) or line:find("^%s*%d%. " .. sym) end
-	local check = function() return line:gsub(EMPTY_BOX, CHECK_BOX, 1) end
-	local clear = function() return line:gsub(CHECK_BOX, EMPTY_BOX, 1) end
-	local make_box = function()
-		if line:match("^%s*-%s.*$") then return line:gsub("(%s*- )(.*)", "%1[ ] %2", 1) end
-		if line:match("^%s*%d%s.*$") then return line:gsub("(%s*%d%. )(.*)", "%1[ ] %2", 1) end
-		return line:gsub("(%S+)", "- [ ] %1", 1)
-	end
-	if has(CHECK_BOX) then return clear() end
-	if has(EMPTY_BOX) then return check() end
-	return make_box()
+  local has = function(box) return line:find("^%s*- " .. box) or line:find("^%s*%d%. " .. box) end
+  local check = function() return line:gsub(EMPTY_BOX, CHECK_BOX, 1) end
+  local clear = function() return line:gsub(CHECK_BOX, EMPTY_BOX, 1) end
+  local make_box = function()
+    if line:match("^%s*-%s.*$") then return line:gsub("(%s*- )(.*)", "%1[ ] %2", 1) end
+    if line:match("^%s*%d%s.*$") then return line:gsub("(%s*%d%. )(.*)", "%1[ ] %2", 1) end
+    return line:gsub("(%S+)", PREFIX .. " %1", 1)
+  end
+  if has(CHECK_BOX) then return clear() end
+  if has(EMPTY_BOX) then return check() end
+  return make_box()
 end
 
 local M = {}
+
 M.toggle = function()
-	local cursor = vim.api.nvim_win_get_cursor(0)
-	local lineid = cursor[1] - 1
-	local line = vim.api.nvim_buf_get_lines(0, lineid, lineid + 1, false)[1] or ""
-	local new_line = toggle_line(line)
-	vim.api.nvim_buf_set_lines(0, lineid, lineid + 1, false, { new_line })
-	vim.api.nvim_win_set_cursor(0, cursor)
+  local vstart, vend = vim.fn.getpos('.')[2], vim.fn.getpos('v')[2]
+  if vstart > vend then vstart, vend = vend, vstart end
+  vstart = vstart - 1
+  local lines = vim.api.nvim_buf_get_lines(0, vstart, vend, false)
+  for i, line in ipairs(lines) do
+    lines[i] = toggle_line(line)
+  end
+  vim.api.nvim_buf_set_lines(0, vstart, vend, false, lines)
 end
 
-vim.api.nvim_create_user_command("ToggleCheckbox", M.toggle, {})
+M.setup = function(opts)
+  if opts.check_sym then CHECK_BOX = "%[" .. opts.check_sym .. "%]" end
+  if opts.prefix then PREFIX = opts.prefix end
+  if opts.prefix_with_box then PREFIX = PREFIX .. " [ ]" end
+end
+
 return M
