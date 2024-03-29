92dd391243f908699c30173c3d1792cc31845562
phanium <nxhsdu@qq.com>
2024-03-29 16:41:51 +0800

    refactor: drop number item support

--- lua/mder/line.lua
+++ lua/mder/line.lua
@@ -1,39 +1,19 @@
 local config = require "mder.config"
 local checkbox = config.check_box
 local emptybox = config.empty_box
-local items = config.list_items
 
-local contain_item = function(line)
-  return vim.iter(items):any(function(i) return line:find("^%s*" .. i) end)
-end
+local contain_item = function(line) return line:find "^%s*[%-%+%*%=]" end
 
-local contain_box = function(line, box)
-  return vim.iter(items):any(function(i) return line:find("^%s*" .. i .. " " .. box) end)
-end
+local contain_box = function(line, box) return line:find("^%s*[%-%+%*%=]%s" .. box) end
 
-local make_box = function(line)
-  for _, i in ipairs(items) do
-    local new_line, ok = line:gsub("^(%s*" .. i .. "%s)(.*)", "%1[ ] %2", 1)
-    if ok == 1 then return new_line end
-  end
-  return line
-end
+local make_box = function(line) return (line:gsub("^(%s*[%-%+%*%=]%s)(.*)", "%1[ ] %2", 1)) end
 
-local make_item = function(line)
-  local new_line, _ = line:gsub("^(%s*)(%S*.*)$", "%1* %2")
-  return new_line
-end
+local make_item = function(line) return (line:gsub("^(%s*)(%S*.*)$", "%1* %2")) end
 
 local toggle_line = function(line)
   if not contain_item(line) then return make_item(line) end
-  if contain_box(line, checkbox) then
-    local new_line, _ = line:gsub(checkbox, emptybox, 1)
-    return new_line
-  end
-  if contain_box(line, emptybox) then
-    local new_line, _ = line:gsub(emptybox, checkbox, 1)
-    return new_line
-  end
+  if contain_box(line, checkbox) then return (line:gsub(checkbox, emptybox, 1)) end
+  if contain_box(line, emptybox) then return (line:gsub(emptybox, checkbox, 1)) end
   return make_box(line)
 end
 
